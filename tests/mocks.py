1335bcb19185f5c487e6f8fe99e162fff093a83c
Mattias.Poppe <poppe.mattias@gmail.com>
2020-09-16 21:40:31 +0200

    dev-1206 Handle s3 delete-object events
    The main callback method has been split up to use a factory pattern
    to determine how to handle the event.
    
    Expand MediahavenService to allow deleting media objects.

--- tests/mocks.py
+++ tests/mocks.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture
 def mock_ftp(monkeypatch):
     def mock_connect(self):
-        print(f"Connecting to host.")
+        print("Connecting to host.")
         pass
 
     def mock_put(self, content_bytes, destination_path, destination_filename):
@@ -31,7 +31,7 @@ def mock_organisations_api(monkeypatch):
 @pytest.fixture
 def mock_events(monkeypatch):
     def mock_init(self, name, ctx):
-        print(f"Initiating Rabbit connection.")
+        print("Initiating Rabbit connection.")
         pass
 
     def mock_publish(self, message, correlation_id):
@@ -50,11 +50,30 @@ def mock_mediahaven_api(monkeypatch):
         print("Getting token.")
         return {"access_token": "bearer, bear with me"}
 
-    def mock_get_fragment(self, query_params):
+    def mock_get_fragment(self, query_params, or_params=False) -> dict:
         print("Get fragment")
         return {
             "MediaDataList": [
-                {"Dynamic": {"PID": "pid"}, "Internal": {"FragmentId": "ID"}}
+                {
+                    "Dynamic": {"PID": "pid", "dc_identifier_localid": "media_id"},
+                    "Internal": {"FragmentId": "ID", "MediaObjectId": "umid", "IsFragment": False}
+                },  # Essence
+                {
+                    "Dynamic": {"PID": "pid2", "dc_identifier_localid": "media_id2"},
+                    "Internal": {"FragmentId": "ID2", "MediaObjectId": "umid", "IsFragment": True}
+                },  # Fragment 1 of essence
+                {
+                    "Dynamic": {"PID": "pid2_collateral", "dc_identifier_localid": "media_id2"},
+                    "Internal": {"FragmentId": "ID3", "MediaObjectId": "umid_collateral", "IsFragment": False}
+                },  # Collateral of fragment 1
+                {
+                    "Dynamic": {"PID": "pid2_metadata", "dc_identifier_localid": "media_id2"},
+                    "Internal": {"FragmentId": "ID4", "MediaObjectId": "umid_metadata", "IsFragment": False}
+                },  # Metadata collateral of fragment 1
+                {
+                    "Dynamic": {"PID": "pid3", "dc_identifier_localid": "media_id3"},
+                    "Internal": {"FragmentId": "ID5", "MediaObjectId": "umid", "IsFragment": True}
+                },  # Fragment 2 of essence
             ]
         }
 
@@ -62,8 +81,13 @@ def mock_mediahaven_api(monkeypatch):
         print("Update metadata")
         pass
 
+    def mock_delete_media_object(self, umid: str) -> bool:
+        print("Delete media object")
+        return True
+
     from meemoo.services import MediahavenService
 
     monkeypatch.setattr(MediahavenService, "_MediahavenService__get_token", mock_get_token)
     monkeypatch.setattr(MediahavenService, "get_fragment", mock_get_fragment)
     monkeypatch.setattr(MediahavenService, "update_metadata", mock_update_metadata)
+    monkeypatch.setattr(MediahavenService, "delete_media_object", mock_delete_media_object)
